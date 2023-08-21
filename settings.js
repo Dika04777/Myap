d05cdd9268f9ba78ff95ef0987850cbda221834b
Dika04777 <112893840+Dika04777@users.noreply.github.com>
2023-08-21 19:27:54 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator ="cc_12dika"// yourname
+global.MONGO_DB_URI = "mongodb+srv://dika04777:<Fr3kCx7y6gD8MQXH>@cluster0.9hvlcbx.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "dika04777@gmail.com" //email
+global.email_password = "dikaaja123" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
